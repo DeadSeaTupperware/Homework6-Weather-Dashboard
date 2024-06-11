8fc3e7a27e2ad8d41bb67a7e24aba007ac82988f
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-11 02:09:24 -0400

    wrote function to call API for 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,15 @@ const apiKey = "87415fb694122c5c0a1eacde1eec7367";
 const searchButton = document.querySelector("#search-button");
 const cityInput = document.querySelector("#city-input");
 
-const getCoordinates = () => {
+function getCoordinates () {
     let cityName = cityInput.value.trim(); // Get user input city name.
-    const apiCall = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+    const apiGeocodingCall = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
     if(!cityName) {
         return;
     }
 
     // Get coordinates from openweathermap API
-    fetch(apiCall)
+    fetch(apiGeocodingCall)
     .then(function (response) {
         return response.json();
     })
@@ -21,6 +21,48 @@ const getCoordinates = () => {
     }).catch(() => {
         alert("An error occurred.");
     });
+}
+
+function getWeather(cityName, lat, lon) {
+    const apiWeatherCall = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(apiWeatherCall)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        const uniqueDays = [];
+        console.log(data);
+
+        // Get one forcast per day
+       const fiveDayForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueDays.includes(forecastDate)) {
+                uniqueDays.push(forecastDate);
+            }
+        });
+
+        console.log(fiveDayForecast);
+        fiveDayForecast.forEach(weatherItem => {
+            createCard(weatherItem);
+        })
+
+    }).catch(() => {
+        alert("An error occurred.");
+    });
+}
+
+function createCard(weatherItem) {
+    return `<li class="card">
+                    <h3>6/11/2024</h3>
+                    <div class="weather-icon">
+
+                    </div>
+                    <h4>Temp: </h4>
+                    <h4>Wind: </h4>
+                    <h4>Humidity: </h4>
+                </li>`;
+
 }
 
 searchButton.addEventListener("click", getCoordinates);
