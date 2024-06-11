ccdba5054f3795d07ec012b64a626a0a1108e119
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-11 00:57:27 -0400

    added javascript to call api and get coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,26 @@
-const APIKey = "87415fb694122c5c0a1eacde1eec7367";
+const apiKey = "87415fb694122c5c0a1eacde1eec7367";
 const searchButton = document.querySelector("#search-button");
+const cityInput = document.querySelector("#city-input");
+
+const getCoordinates = () => {
+    let cityName = cityInput.value.trim(); // Get user input city name.
+    const apiCall = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+    if(!cityName) {
+        return;
+    }
+
+    // Get coordinates from openweathermap API
+    fetch(apiCall)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        if(!data.length) return alert( `${cityName} was not found.`);
+        const { name, lat, lon } = data[0];
+        getWeather(name, lat, lon);
+    }).catch(() => {
+        alert("An error occurred.");
+    });
+}
+
+searchButton.addEventListener("click", getCoordinates);
